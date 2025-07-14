2aab0aebd342c5b0189902d14e42e7d7cc9ce23f
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-14 09:40:19 +0530

    stats of file

--- Modules/FileHandling/File.js
+++ Modules/FileHandling/File.js
@@ -23,5 +23,14 @@ const fs = require("fs");
 
 // fs.appendFileSync("./contact.txt", `${Date.now()}Hi Sachin \n`);
 
-fs.cpSync("./contact.txt","./copyFile.txt");
+// fs.cpSync("./contact.txt","./copyFile.txt");
+
+//delete file
+
+// fs.unlinkSync("./copyFile.txt")
+
+// stats 
+
+console.log(fs.statSync("./contact.txt"))
+
 
