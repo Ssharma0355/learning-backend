5982d1c3e193004510aaad7215bb359c3de4c45a
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-14 09:29:20 +0530

    read sync and async

--- Modules/FileHandling/File.js
+++ Modules/FileHandling/File.js
@@ -7,8 +7,15 @@ const fs = require("fs");
 //async
 // fs.writeFile("./test2.txt", "Async file handling",(err)=>{});
 
-//Read the file
+//Read the file sync
 
-const contactResult = fs.readFileSync("./contact.txt","utf-8");
-console.log(contactResult);
+// const contactResult = fs.readFileSync("./contact.txt","utf-8");
+// console.log(contactResult);
+
+const results = fs.readFile("./contact.txt","utf-8",(err,result)=>{
+    if(err){}
+    else if(result){
+        console.log(result)
+    }
+})
 
